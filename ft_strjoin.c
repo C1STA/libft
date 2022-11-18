f11681ee585399c52f5d526d4046c9bb681dc92d
C1STA <42x856@gmail.com>
2022-11-18 16:44:30 +0100

    first commit

---
+++ ft_strjoin.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wcista <wcista@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/30 13:33:46 by wcista            #+#    #+#             */
+/*   Updated: 2022/06/30 17:43:03 by wcista           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*dest;
+	size_t	len;
+	int		i;
+	int		j;
+
+	if (!(s1 || s2))
+		return (NULL);
+	len = (ft_strlen(s1) + ft_strlen(s2) + 1);
+	dest = malloc((len) * sizeof(char));
+	if (!dest)
+		return (NULL);
+	i = -1;
+	j = 0;
+	while (s1[++i])
+		dest[j++] = s1[i];
+	i = -1;
+	while (s2[++i])
+		dest[j++] = s2[i];
+	dest[j] = '\0';
+	return (dest);
+}
